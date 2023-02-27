ff624be46cae47d0c6b275bfac122c5ecc53e1ac
Robert Jordan <rjordan1994@gmail.com>
2023-02-27 14:40:04 -0800

    My Code!

--- 010-typeof/scripts/main.js
+++ 010-typeof/scripts/main.js
@@ -1,23 +1,42 @@
-// Global Scope
-let myName = "Robert"
-let myAge = 28
-let myHappiness = true
-let newVar = 'Something cool'
-const myConst = 42
-console.log(myAge == myName)
-console.log(myAge == myName)
+// // Global Scope
+// let myName = "Robert"
+// let myAge = 28
+// let myHappiness = true
+// let newVar = 'Something cool'
+// const myConst = 42
+// console.log(myAge == myName)
+// console.log(myAge == myName)
 
-let myVar = null
-console.log(`myVar is typeof ${typeof myVar}`)
+// let myVar = null
+// console.log(`myVar is typeof ${typeof myVar}`)
 
-console.log(`myName is typeof ${typeof myName}`)
-console.log(`myName is value  ${myName}`)
-console.log(`myAge is typeof ${typeof myAge}`)
-console.log(`myHappiness is typeof ${typeof myHappiness}`)
+// console.log(`myName is typeof ${typeof myName}`)
+// console.log(`myName is value  ${myName}`)
+// console.log(`myAge is typeof ${typeof myAge}`)
+// console.log(`myHappiness is typeof ${typeof myHappiness}`)
 
-//Block Scope
-if (true) {
-    const myConst = 43
-    console.log(myConst)
+// //Block Scope
+// if (true) {
+//     const myConst = 43
+//     console.log(myConst)
 
-}
+// }
+
+
+//My Code
+
+let myDad = "Ralph"
+let myDadAge = 61
+let myMom = "Gina"
+let myMomAge = 58
+let newVar = "I love my parents."
+const myConst = 22
+console.log(myDad == myDadAge)
+console.log(myMom == myMomAge)
+
+console.log(`myDad is typeof ${typeof myDad}`)
+console.log(`myDad is value  ${myDad}`)
+console.log(`myDadAge is value  ${myDadAge}`)
+console.log(`myMom is typeof ${typeof myMom}`)
+console.log(`myMom is value  ${myMom}`)
+console.log(`myMomAge is value  ${myMomAge}`)
