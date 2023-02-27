e29385a7692989ff62e1426dc474200515fbc829
Robert Jordan <rjordan1994@gmail.com>
2023-02-27 15:29:52 -0800

    My Arrow Function.

--- 011-functions/main.js
+++ 011-functions/main.js
@@ -1,18 +1,31 @@
-function doMath (a, b) {
-    let result = a * b
-    return result
-    // console.log(`this is the result ${a * b}`)
-    // console.log(`this is the result ${a + b}`)
-    // console.log(`this is the result ${a - b}`)
-    // console.log(`this is the result ${a / b}`)
+// function doMath (a, b) {
+//     let result = a * b
+//     return result
+//     // console.log(`this is the result ${a * b}`)
+//     // console.log(`this is the result ${a + b}`)
+//     // console.log(`this is the result ${a - b}`)
+//     // console.log(`this is the result ${a / b}`)
 
-}
-doMoreMath = (a , b) => a * b
+// }
+// doMoreMath = (a , b) => a * b
     
 
 
-console.log(doMoreMath(20, 30))
-console.log(result)
+// console.log(doMoreMath(20, 30))
+// console.log(result)
 
-const result = doMath(10, 4)
-console.log(`this result was returned from the function ${result}`)
+// const result = doMath(10, 4)
+// console.log(`this result was returned from the function ${result}`)
+
+function myMath (a, b, c) {
+    let result = a * b - c
+    return result
+}
+
+console.log(myMath(10,20,30))
+ 
+
+const result = myMath(10, 20, 30)
+console.log(`this is the result of the function ${result}`)
+// End result should be 170
+
