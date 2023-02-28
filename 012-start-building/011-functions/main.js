574987783fd2019935a795d902495e58f18d4d44
Robert Jordan <rjordan1994@gmail.com>
2023-02-27 22:30:48 -0800

    My Array

--- 012-start-building/011-functions/main.js
+++ 012-start-building/011-functions/main.js
@@ -1,5 +1,5 @@
 //Update  the DOM
-const myArr = ['Robert', 29]
+const myArr = []
 
 
 const updateDOM = (input) => {
@@ -10,12 +10,16 @@ const updateDOM = (input) => {
     divEl.appendChild(p)
 }
 
-const trackMPGCost = (miles, gallons, price = 3.79) => {
+const trackMPGCost = (miles, gallons, price) => miles/gallons * price
+const miles = parseInt(prompt('Enter the number of miles you want to travel:'))
+const gallons = parseInt(prompt('Enter the number of gallons your vehicle can hold:'))
+const price = parseFloat(prompt('Enter the price per gallon of gas:'))
 const MPG = miles/gallons
 const tripCost = MPG * price
 myArr.push(MPG, tripCost)
-}
 
-updateDOM(trackMPGCost(300, 10, 5.40))
-updateDOM(trackMPGCost(320, 12, 5))
 
+updateDOM(trackMPGCost(miles, gallons, price))
+updateDOM(trackMPGCost(miles, gallons, price))
+
+// Added the lesson on to my previous code to see if it would work
