9060a48cac1de465335650e1e098a075c23a1c49
Robert Jordan <rjordan1994@gmail.com>
2023-02-27 20:44:46 -0800

    Updated DOM My Changes

--- 012-start-building/011-functions/main.js
+++ 012-start-building/011-functions/main.js
@@ -1,11 +1,32 @@
 //Update  the DOM
 
+// const updateDOM = () => {
+
+//     const divEl = document.querySelector('#output')
+//     const p = document.createElement('p')
+//     p.textContent = 'output to browse'
+//     divEl.appendChild(p)
+// }
+
+// updateDOM(100)
+
+
+// My Updated DOM
 const updateDOM = () => {
 
     const divEl = document.querySelector('#output')
     const p = document.createElement('p')
+    const divE2 = document.querySelector('#body')
+    const body = document.createElement('body')
+    body.textContent = 'this is my body'
     p.textContent = 'output to browse'
     divEl.appendChild(p)
+    divE2.appendChild(body)
+
+    console.log(p.textContent)
+    console.log(body)
+    
+// Displays body content underneath divE1 Output.
 }
 
 updateDOM(100)
