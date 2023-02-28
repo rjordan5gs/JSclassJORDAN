723cc2f931bfbdbb65452c1691c8f0b0faba7cba
Robert Jordan <rjordan1994@gmail.com>
2023-02-27 22:05:22 -0800

    My Mileage Program

--- 012-start-building/011-functions/main.js
+++ 012-start-building/011-functions/main.js
@@ -9,7 +9,13 @@ const updateDOM = (input) => {
 }
 
 const mileage = (miles, gallons, price) => miles/gallons * price
+const miles = parseInt(prompt('Enter the number of miles you want to travel:'));
+const gallons = parseInt(prompt('Enter the number of gallons your vehicle can hold:'));
+const price = parseFloat(prompt('Enter the price per gallon of gas:'));
 
 
+updateDOM(mileage(miles, gallons, price))
 
-updateDOM(mileage(300, 10, 3.79))
+//Took inspiration from what you said in the lecture about User Input Data, The values in
+//updateDOM section were changed to reflect user input to be calulated instead of set
+//values.
