31605d9b905454014d9112430881a63f6214ae90
Robert Jordan <rjordan1994@gmail.com>
2023-02-27 21:05:14 -0800

    Starting Mileage Program.

--- 012-start-building/011-functions/main.js
+++ 012-start-building/011-functions/main.js
@@ -1,32 +1,15 @@
 //Update  the DOM
 
-// const updateDOM = () => {
-
-//     const divEl = document.querySelector('#output')
-//     const p = document.createElement('p')
-//     p.textContent = 'output to browse'
-//     divEl.appendChild(p)
-// }
-
-// updateDOM(100)
-
-
-// My Updated DOM
-const updateDOM = () => {
+const updateDOM = (input) => {
 
     const divEl = document.querySelector('#output')
     const p = document.createElement('p')
-    const divE2 = document.querySelector('#body')
-    const body = document.createElement('body')
-    body.textContent = 'this is my body'
-    p.textContent = 'output to browse'
+    p.textContent = input
     divEl.appendChild(p)
-    divE2.appendChild(body)
-
-    console.log(p.textContent)
-    console.log(body)
-    
-// Displays body content underneath divE1 Output.
 }
 
-updateDOM(100)
+const mileage = (miles, gallons, price) => miles/gallons * price
+
+
+
+updateDOM(mileage(300, 10, 3.79))
