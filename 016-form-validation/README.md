bba30320edcbc1d704b18e4b4710a566f7ae8619
Robert Jordan <rjordan1994@gmail.com>
2023-03-02 15:21:35 -0800

    Solution code

--- 016-form-validation/README.md
+++ 016-form-validation/README.md
@@ -1,3 +1,25 @@
 ## Week 8 Form Validation
 
-- Questions
+# Does the "if else" block make sense to you?
+
+- Yes it does, in the block, the code means if miles is equal to zero activate the bracket code "console.log(errmesage)" if the if statement is true, otherwise use the else block, trackMPGandCost(miles, gal, pric) , and run through the code.
+
+- I had a question about getElementById and querySelector, Why is it better to use getElementById vs querySelector? Is it more specific? Lastly, why is
+  getElementById in the global scope vs querySelector in UpdateDOM function?
+
+# Was I successful in the two challenges?
+
+- I was not successful in clearing the error message, it seems like I'm having some trouble with live server, after I submit my userdata the rest of the code runs and the server refreshes, I tried looking up solutions, but to no avail.
+
+My solution would have been to set and if statement to the condition that:
+"if this statement was true, to remove the error message." then set it up in the global scope.
+
+- For the solution displaying the average to the DOM, I added an if stattement
+  and an array, the if statement is as follows:
+
+"if MY_TRIP_AVG is greater than [0], then updateDOM with avgTripCost."
+
+I realize this does not take into account the stored data, but if the data was
+activly being stored I would use the data in the array to determine what information was being displayed to the DOM.
+
+- Extra Comment (my reaction to your solution: "Wow, no duhh!")
--- 016-form-validation/main.js
+++ 016-form-validation/main.js
@@ -1,15 +1,16 @@
 //Update  the DOM
-const FORM = document.getElementById(`form-input`)
+const FORM = document.getElementById('form-input')
 const ERR = document.getElementById('err')
+const AVG_OUTPUT = document.getElementById('output-avg')
 
 const MY_MPG = []
 const MY_TRIP_COST = []
-const MY_TRIP_AVG=[]
 
 
-const updateDOM = (input) => {
 
-    const divEl = document.querySelector('#output')
+const updateDOM = (input, id) => {
+
+    const divEl = document.querySelector(id)
     const p = document.createElement('p')
     p.textContent = input
     divEl.appendChild(p)
@@ -18,7 +19,7 @@ const updateDOM = (input) => {
 const trackMPGandCost = (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons)
     const tripCost = Math.round(gallons *price)
-    updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
+    updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`, ('#output'))
     MY_MPG.push(MPG)
     MY_TRIP_COST.push(tripCost)
 
@@ -36,15 +37,9 @@ const calculateAvg = () => {
         let sumTripCost = calculateSUM(MY_TRIP_COST)
         let avgMPG = Math.round(sumMPG/MY_MPG.length)
         let avgTripCost = Math.round(sumTripCost/MY_TRIP_COST.length)
-        updateDOM(`Average MPG is ${avgMPG}`)
+        updateDOM(`Average MPG is ${avgMPG}`, '#output-avg')
         updateDOM(`Average trip cost is ${avgTripCost}`)
-        MY_TRIP_AVG.push(avgTripCost)
-            if (MY_TRIP_COST < [0]) {
-                updateDOM(`Average trip cost is ${avgTripCost}`)
-            }
-
-
-}
+ }
 
 FORM.addEventListener('submit', (e) => {
     e.preventDefault
@@ -52,16 +47,19 @@ FORM.addEventListener('submit', (e) => {
     const miles = parseInt(e.target.miles.value)
     const gallon = parseInt(e.target.gallons.value)
     const price = parseInt(e.target.price.value)
-    if(price > 1000) {
+    if(miles === 0 || gallons === 0 || price === 0 ) {
         errMsg.push('Make sure your input value is greater than 0!, Try Again.')
     } 
 
     if(errMsg.length > 0) {
         ERR.textContent = errMsg
     } else {
+        ERR.textContent = ''
+        AVG_OUTPUT.textContent = ''
             trackMPGandCost(miles, gallon, price)
             calculateAvg()
     }
+    FORM.reset()
     
 })
 
