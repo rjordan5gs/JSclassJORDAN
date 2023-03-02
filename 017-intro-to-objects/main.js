19a7ade40523b36e2857f5c623a9925587c2204e
Robert Jordan <rjordan1994@gmail.com>
2023-03-01 20:07:50 -0800

    intro to objects

---
+++ 017-intro-to-objects/main.js
@@ -0,0 +1,21 @@
+const myArr =[]
+
+// console.log(typeof myObj)
+// console.log(typeof myArr)
+// console.log(myArr === myObj)
+const myNumber = 9
+const myObj ={
+    name: 'Rio',
+    age: 59,
+    hobbies: ['coding', 'running' , 'workout'],
+    mobile: {
+        make: 'pixel 7',
+        OS: 'android'
+    },
+    myGreeting: function(person) {
+        return `what is up ${person}`
+    }
+}
+
+const greeting = myObj.greeting('jane')
+console.log(greeting)
