06c75df84e162efbe1f7126753eba0cfdbcd302a
Robert Jordan <rjordan1994@gmail.com>
2023-03-02 17:35:15 -0800

    This is mine

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -1,56 +1,75 @@
+// global
 const CHAR_SELECT = document.getElementById('characters')
 const FORM = document.getElementById('form-input')
 const OUTPUT_DIV = document.getElementById('output')
 
-const updateDOM = (input) => {
-    const p = document.createElement('p')
-    p.textContent = input
-    OUTPUT_DIV.appendChild(p)
-}
 
+// primitives
+const strength = 10;
+const dexterity = 8;
+const agility = 4;
+const intelligence = 2;
+const warriorWeapons = ['Bastard Sword', 'Axe', 'Fists'];
+const rangerWeapons = ['Long Bow', 'Long Sword', 'Dagger'];
+// booleans
+const isWarrior = true;
+const isRanger = false;
+// numbers
+const warriorLevel = 1;
+const rangerLevel = 1;
+// arrays
+const stats = [strength, dexterity, agility, intelligence];
+const characters = ['Warrior', 'Ranger'];
+// methods
+const myCharacter = function() {
+  return `You have chosen ${this.class}, your weapon of choice is ${this.weapons[0]}.`;
+}
+// functions
+const updateDOM = (input) => {
+  const p = document.createElement('p')
+  p.textContent = input
+  OUTPUT_DIV.appendChild(p)
+}
+// objects
 const warriorCharacter = {
-    class: 'Warrior',
-    level: 1,
-    weapons: ['Bastard Sword', 'Axe' , 'Fists'],
-    Stats: {
-        Strength: '10',
-        Dexterity: '8',
-        Agility:'4',
-        Intellegence:'2'
-    },
-    myCharacter: function() {
-        return `You have chosen ${this.class}, your weapon of choice is ${this.weapons[0]}.`
-    }
+  class: 'Warrior',
+  level: warriorLevel,
+  weapons: warriorWeapons,
+  Stats: {
+    Strength: strength,
+    Dexterity: dexterity,
+    Agility: agility,
+    Intellegence: intelligence
+  },
+  myCharacter: myCharacter
 }
 
 const rangerCharacter = {
-    class: 'Ranger',
-    level: 1,
-    weapons: ['Long Bow', 'Long Sword' , 'Dagger'],
-    Stats: {
-        Strength: '7',
-        Dexterity: '5',
-        Agility:'10',
-        Intellegence:'6'
-    },
-    myCharacter: function() {
-        return `You have chosen ${this.class}, your weapon of choice is ${this.weapons[0]}.`
-    }
+  class: 'Ranger',
+  level: rangerLevel,
+  weapons: rangerWeapons,
+  Stats: {
+    Strength: 7,
+    Dexterity: 5,
+    Agility: 10,
+    Intellegence: 6
+  },
+  myCharacter: myCharacter
 }
-
+// event listener
 FORM.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const selectedCharacter = CHAR_SELECT.value
-    let character
+  e.preventDefault()
+  const selectedCharacter = CHAR_SELECT.value
+  let character
 
-    if (selectedCharacter === 'Warrior') {
-        character = warriorCharacter
-    } else if (selectedCharacter === 'Ranger') {
-        character = rangerCharacter
-    }
+  if (selectedCharacter === 'Warrior') {
+    character = warriorCharacter
+  } else if (selectedCharacter === 'Ranger') {
+    character = rangerCharacter
+  }
 
-    if (character) {
-        const characterInfo = character.myCharacter()
-        updateDOM(characterInfo)
-    }
+  if (character) {
+    const characterInfo = character.myCharacter()
+    updateDOM(characterInfo)
+  }
 })
