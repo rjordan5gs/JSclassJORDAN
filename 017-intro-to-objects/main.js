c30f2dbcd5b3ef36b2f8987547cc933779855538
Robert Jordan <rjordan1994@gmail.com>
2023-03-17 16:52:14 -0700

    += Objects

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -1,75 +1,38 @@
-// global
-const CHAR_SELECT = document.getElementById('characters')
-const FORM = document.getElementById('form-input')
-const OUTPUT_DIV = document.getElementById('output')
+const myArr = []
 
 
-// primitives
-const strength = 10;
-const dexterity = 8;
-const agility = 4;
-const intelligence = 2;
-const warriorWeapons = ['Bastard Sword', 'Axe', 'Fists'];
-const rangerWeapons = ['Long Bow', 'Long Sword', 'Dagger'];
-// booleans
-const isWarrior = true;
-const isRanger = false;
-// numbers
-const warriorLevel = 1;
-const rangerLevel = 1;
-// arrays
-const stats = [strength, dexterity, agility, intelligence];
-const characters = ['Warrior', 'Ranger'];
-// methods
-const myCharacter = function() {
-  return `You have chosen ${this.class}, your weapon of choice is ${this.weapons[0]}.`;
-}
-// functions
-const updateDOM = (input) => {
-  const p = document.createElement('p')
-  p.textContent = input
-  OUTPUT_DIV.appendChild(p)
-}
-// objects
-const warriorCharacter = {
-  class: 'Warrior',
-  level: warriorLevel,
-  weapons: warriorWeapons,
-  Stats: {
-    Strength: strength,
-    Dexterity: dexterity,
-    Agility: agility,
-    Intellegence: intelligence
+
+
+const myNumber = 9
+const myObj = {
+  name: `Rio`,
+  age: 59,
+  hobbies: [`coding`, `running`, `workout`],
+  mobile: {
+    make: `pixel 7`,
+    OS: `android`
   },
-  myCharacter: myCharacter
-}
-
-const rangerCharacter = {
-  class: 'Ranger',
-  level: rangerLevel,
-  weapons: rangerWeapons,
-  Stats: {
-    Strength: 7,
-    Dexterity: 5,
-    Agility: 10,
-    Intellegence: 6
+  myGreeting: function(person) {
+    return `$(this.name) who loves to $(this.hobbies[0]) says What is UP $(person)`
   },
-  myCharacter: myCharacter
+  increaseLikes: function() {
+    this.likes += 1
+  }
 }
-// event listener
-FORM.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const selectedCharacter = CHAR_SELECT.value
-  let character
+const rioObj = myObj
+rioObj.increaseLikes()
+console.log(rioObj)
+rioObj.age = 60
 
-  if (selectedCharacter === 'Warrior') {
-    character = warriorCharacter
-  } else if (selectedCharacter === 'Ranger') {
-    character = rangerCharacter
-  }
 
-  if (character) {
-    const characterInfo = character.myCharacter()
-    updateDOM(characterInfo)
-  }
-})
+
+
+const greeting = myObj.myGreeting(`jane`)
+console.log(greeting)
+
+console.log(myObj.mobile.make.length)
+
+for(key in myObj) {
+  console.log(`key ${key} and value ${myObj[key]}`)
+}
+
