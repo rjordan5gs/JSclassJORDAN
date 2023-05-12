8a21edcb02169151864a52f3172df49a8c49dc64
rjordan5gs <rjordan1994@gmail.com>
2023-05-11 17:21:31 -0700

    Added more functionality and userObj for data manipulation.

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,6 +1,9 @@
 //global variables
 const RECORDS = [];
 
+
+
+
 //functions
 function updateDOM(input, id) {
   const outputEl = document.querySelector(id);
@@ -8,37 +11,46 @@ function updateDOM(input, id) {
   outputEl.textContent = recordText;
 }
 
+
+
+
 function addRecord(event) {
-    event.preventDefault();
-  
-    const mood = document.querySelector('#mood').value;
-    const timeOfDay = document.querySelector('#time-of-day').value;
-    const goodDay = document.querySelector('#good-day').value;
-  
-    const record = { mood, timeOfDay, goodDay };
-        RECORDS.push(record);
-        updateDOM(record, '#output');
-  
-        if (mood === 'Happy' && goodDay === 'Yes') {
-            displayMessage("Sick! Keep on being productive!");
-        } else {
-            displayMessage("It's going to be hard, but keep up the hard work!");
-        }
-  
-        document.querySelector('#mood').value = '';
-        document.querySelector('#time-of-day').value = '';
-        document.querySelector('#good-day').value = '';
-  }
-  
+	event.preventDefault();
+ 
+	const mood = document.querySelector('#mood').value;
+	const timeOfDay = document.querySelector('#time-of-day').value;
+	const goodDay = document.querySelector('#good-day').value;
+ 
+	const record = { mood, timeOfDay, goodDay };
+	RECORDS.push(record);
+	updateDOM(record, '#output');
+ 
+	const userObj = {
+    	data: record,
+    	decision: mood === 'Happy' && goodDay === 'Yes' ? "Sick! Keep on being productive!" : "It's going to be hard, but keep up the hard work!"
+	}
+	displayMessage(userObj.decision);
+ 
+	document.querySelector('#mood').value = '';
+	document.querySelector('#time-of-day').value = '';
+	document.querySelector('#good-day').value = '';
+}
+
+
+
 
 function displayMessage(message) {
-    const messageEl = document.querySelector('#message');
-    messageEl.textContent = message;
-  }
+	const messageEl = document.querySelector('#message');
+	messageEl.textContent = message;
+}
 
-  
+
+
+
 const form = document.querySelector('form');
 form.addEventListener('submit', addRecord);
+
+
 
 
 
