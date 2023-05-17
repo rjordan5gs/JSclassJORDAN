7302d1ff31882edf68c991cb67d19c3d587b43c9
rjordan5gs <rjordan1994@gmail.com>
2023-05-17 15:58:21 -0700

    getting weather in fresno

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -76,7 +76,7 @@
 
 function fetchData() {
     return new Promise(function(resolve, reject) {
-        fetch('https://api.weather.gov/gridpoints/OKX/35,25/forecast')
+        fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
         .then(response => response.json())
         .then(data.properties.periods[1].shortForecast)
     })
