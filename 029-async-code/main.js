534821d44eeebd2dfdeed586978d6e90d4e25093
rjordan5gs <rjordan1994@gmail.com>
2023-05-16 21:42:56 -0700

    async code

--- 029-async-code/main.js
+++ 029-async-code/main.js
@@ -1,14 +1,53 @@
-let pizza
 
-function orderPizza() {
-    console.log('Order Pizza')
-    setTimeout(() => {
-        
-    }, 2000);
-    pizza = '🍕'
-    console.log ('Pizza was ordered')
+// let pizza
+
+// function orderPizza() {
+//     console.log('Order Pizza')
+//     setTimeout(() => {
+//         pizza = '🍕'
+//         console.log (`${pizza} is ready`)
+//     }, 2000);
+//     console.log('Pizza was ordered')
+// }
+
+// orderPizza()
+// console.log('Call Qoli')
+// console.log(`Eat ${pizza}`)
+
+// Async
+
+// function orderPizza(callback) {
+//     setTimeout(() => {
+//         const pizza = `🍕`
+//         callback()
+//     }, 2000);
+// }
+
+// function pizzaReady() {
+//     console.log(`eat the ${pizza}`)
+// }
+
+// orderPizza(pizzaReady)
+// console.log(`Call Qoli`)
+
+// window.addEventListener('click', callback) {
+//     console.log('Clicked')
+// }
+
+function thing1(callback) {
+    callback()
 }
 
-orderPizza()
-console.log(`Eat ${pizza}`)
+function thing2(callback) {
+    callback()
+}
 
+function thing3(callback) {
+    callback()
+}
+
+thing1(() => {
+    thing2(() => {
+        thing3()
+    })
+})
