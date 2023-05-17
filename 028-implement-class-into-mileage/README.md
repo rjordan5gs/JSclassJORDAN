6e88b8fdbc43c22dda2e2497bb03154e60ca2647
rjordan5gs <rjordan1994@gmail.com>
2023-05-16 18:55:33 -0700

    my attempt

---
+++ 028-implement-class-into-mileage/README.md
@@ -0,0 +1,6 @@
+# Week 13 Implement Class into Mileage
+
+I decided to try and put my class in handleinput.js and called it Trip. Since trackMPGandCost was the only function with an obvious object i decided to try and encapsulate it functionality into the class.
+
+Things that broke:
+
---
+++ 028-implement-class-into-mileage/handleinput.js
@@ -0,0 +1,75 @@
+const ERR = document.getElementById('err')
+const AVG_OUTPUT = document.getElementById('output-avg')
+
+
+
+function updateDOM (input, id) {
+    const divEl = document.querySelector(id)
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+
+
+// function trackMPGandCost (miles, gallons, price) {
+//     const MPG  = Number((miles/gallons).toFixed(2))
+//     const tripCost = Number((gallons * price).toFixed(2))
+//     updateDOM(`Miles per gallon  is ${MPG} and trip cost is ${tripCost}`, '#output')
+//     return {
+//         miles: miles,
+//         gallons: gallons,
+//         price: price,
+//         MPG: MPG, 
+//         tripCost: tripCost
+//     }
+// }
+
+class MPGandCost {
+    constructor(miles,gallons, price) {
+        this._miles = miles
+        this._gallons = gallons
+        this._price = price
+        this._MPG = Number((miles/gallons).toFixed(2))
+        this._tripCost = Number((gallons * price).toFixed(2))
+    }
+}
+
+
+
+function calculateAvg (MY_DATA) {
+    AVG_OUTPUT.innerHTML = ''
+    const numberOfObj = MY_DATA.length
+    const sums = MY_DATA.reduce(function(sum, obj){
+        return {
+             MPG: sum.MPG + obj.MPG,
+             tripCost: sum.tripCost + obj.tripCost
+        }
+    })
+    const avgMPG = Number((sums.MPG/numberOfObj).toFixed(2))
+    const avgTripCost = Number((sums.tripCost/numberOfObj).toFixed(2))
+    updateDOM(`Average MPG is ${avgMPG}`, '#output-avg')
+    updateDOM(`Average Trip Cost is ${avgTripCost}`, '#output-avg')
+}
+
+
+
+function isFormValid (miles, gallons, price) {
+    const errMsg = []
+    if (miles === 0 || gallons === 0 || price === 0) {
+        errMsg.push('Make sure your input value greater than 0!!, Try Again')
+    }
+    if (price > 1000) {
+        errMsg.push('Really!!!? I think this is in error...Try again')
+    }
+    if (errMsg.length > 0) {
+        ERR.textContent = errMsg
+        return false
+    } else {
+        return true
+    }
+}
+
+
+
+export {isFormValid, calculateAvg, MPGandCost}
