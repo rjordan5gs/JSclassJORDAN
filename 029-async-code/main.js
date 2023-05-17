8932dc60fefe27a23a6788f29eb58101a61b1b33
rjordan5gs <rjordan1994@gmail.com>
2023-05-16 21:30:06 -0700

    syn code

---
+++ 029-async-code/main.js
@@ -0,0 +1,14 @@
+let pizza
+
+function orderPizza() {
+    console.log('Order Pizza')
+    setTimeout(() => {
+        
+    }, 2000);
+    pizza = '🍕'
+    console.log ('Pizza was ordered')
+}
+
+orderPizza()
+console.log(`Eat ${pizza}`)
+
