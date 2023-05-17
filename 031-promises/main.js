cc34a4cfdc3a4f8b1ecec15b501e6272c9856dc0
rjordan5gs <rjordan1994@gmail.com>
2023-05-17 15:08:04 -0700

    More promise methods

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -1,38 +1,76 @@
-function getWeather () {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            resolve('Sunny')
-        }, 100);
-    })
-}
+// function getWeather () {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             reject('Rainy')
+//         }, 100);
+//     })
+// }
 
-function getWeatherIcon(weather) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            switch(weather) {
-                case 'Sunny':
-                    resolve('sun')
-                    break
-                case 'Cloudy':
-                    resolve('Cloud')
-                    break
-                case 'Rainy':
-                    resolve('Rain')
-                default:
-                    reject('NO ICON FOUND')
-            }
-        }, 100);
-    })
-}
+// function getWeatherIcon(weather) {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             switch(weather) {
+//                 case 'Sunny':
+//                     resolve('sun')
+//                     break
+//                 case 'Cloudy':
+//                     resolve('Cloud')
+//                     break
+//                 case 'Rainy':
+//                     resolve('Rain')
+//                 default:
+//                     reject('NO ICON FOUND')
+//             }
+//         }, 100);
+//     })
+// }
 
-function onSuccess(data) {
-    console.log(`Sucess ${data}`)
-}
+// function onSuccess(data) {
+//     console.log(`Sucess ${data}`)
+// }
 
-function onError(error) {
-    console.log(`Error: ${error}`)
-}
+// function onError(error) {
+//     console.log(`Error: ${error}`)
+// }
 
-getWeather()
-    .then(getWeatherIcon)
-    .then(onSuccess, onError)
+// getWeather()
+//     .then(getWeatherIcon)
+//     .then(onSuccess, onError)
+//     .catch(onError)
+
+
+// function fun1() {
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             resolve('Good Data')
+//         }, 100);
+//     })
+// }
+
+// function fun2() {
+//     console.log('Function 2')
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             resolve('Mustache')
+//         }, 100);
+//     })
+// }
+
+// function onSuccess(data) {
+//     console.log(`Success: ${data}`)
+// }
+
+// function onError(errorCode) {
+//     console.log(`ERROR: ${errorCode}`)
+// }
+
+// function onFinally() {
+//     console.log("We are done")
+// }
+
+// fun1()
+//     .then(fun2, onError)
+//     .then(onSuccess)
+//     .catch(onError)
+//     .finally(onFinally)
+
