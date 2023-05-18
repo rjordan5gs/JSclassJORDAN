6022a2fdde0bb53781fe6fa9d2b1d6fb273cfc02
rjordan5gs <rjordan1994@gmail.com>
2023-05-18 01:27:34 -0700

    fetch header object

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,32 +1,54 @@
-// // const url = 
-// // 'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
+// // // const url = 
+// // // 'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
 
-// // async function getData() {
-// //     const response = await fetch(url)
-// //     const data = await response.json()
-// //     console.log(data)
-// // }
+// // // async function getData() {
+// // //     const response = await fetch(url)
+// // //     const data = await response.json()
+// // //     console.log(data)
+// // // }
 
-// // getData()
+// // // getData()
 
-const url = 
-'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
+// const url = 
+// 'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
 
-api_token='no token here'
+// api_token='no token here'
+
+// const request = new Request(url, {
+//         headers: {
+//             'Authorization': 'Bearer'
+//         }
+//     })
+
+// async function getData() {
+//     const responce = await fetch(request)
+//     const data = await responce.json()
+
+// }
+
+// getData()
+
+const url = 'http://banana.pudding'
 
 const request = new Request(url, {
-        headers: {
-            'Authorization': 'Bearer'
-        }
-    })
+    headers: {
+        'Authorization': 'Bearer 123'
+    }
+})
 
 async function getData() {
-    const responce = await fetch(request)
-    const data = await responce.json()
-
+    try {
+        const responce = await fetch(request)
+        const data = await responce.json()
+        if (responce.status !== 200) {
+            console.log('Success', data)
+        } else{
+            console.log('Server Error', data.error.message)
+        }
+    } catch(error) {
+        console.log('Fetch Error', error)
+    }
 }
 
 getData()
-
-
 
