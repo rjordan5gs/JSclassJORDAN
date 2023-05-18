22a04dd31e3cf14593ddb10defcf4799a1215e54
rjordan5gs <rjordan1994@gmail.com>
2023-05-17 19:15:42 -0700

    try/catch

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,40 +1,28 @@
-// function getData() {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve(46)
-//         }, 1);
-//     })
-// }
 
-
-// async function start() {
-// const result = await getData()
-// console.log(result)
-// }
-
-// async function start2() {
-//     getData()
-//         .then(result => {
-//             console.log(result)
-//         })
-//         .catch()
-// }
-
-// start()
+function getData () {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            reject('Somthing went wrong!')
+        }, 1);
+    })
+}
 
 async function start() {
-    const data = await fetch()
-    const result = await data.json()
-    console.log(result.properties.periods[1].shortForecast)
+    try {
+        const result = await getData()
+    } catch (error) {
+        console.log(`ERROR: ${error}`)
+    }
+    console.log(result)
 }
 
 async function start2() {
-    fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
-        .then(data => data.json())
-        .then(result => {
-            console.log(result.properties.periods[1].shortForecast)
-        })
+    const result = await getData()
+    .catch( error => {
+        console.log(`ERROR: ${error}`)
+    })
+    console.log(result)
 }
+start2
 
 start()
-
