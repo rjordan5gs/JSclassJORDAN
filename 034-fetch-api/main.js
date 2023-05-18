22fbb9ff5ec535abc819ae9b1c7d992ceea4cd65
rjordan5gs <rjordan1994@gmail.com>
2023-05-18 01:26:22 -0700

    API keys

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,10 +1,32 @@
+// // const url = 
+// // 'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
+
+// // async function getData() {
+// //     const response = await fetch(url)
+// //     const data = await response.json()
+// //     console.log(data)
+// // }
+
+// // getData()
+
 const url = 
 'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
 
+api_token='no token here'
+
+const request = new Request(url, {
+        headers: {
+            'Authorization': 'Bearer'
+        }
+    })
+
 async function getData() {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
+    const responce = await fetch(request)
+    const data = await responce.json()
+
 }
 
 getData()
+
+
+
